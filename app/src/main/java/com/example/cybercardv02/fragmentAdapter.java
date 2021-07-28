9651f0fd0769f961b020bc33f57035f3d620354f
Blane Staskiewicz <bas6248@psu.edu>
2021-07-28 11:42:49 -0400

    Update fragmentAdapter.java

--- app/src/main/java/com/example/cybercardv02/fragmentAdapter.java
+++ app/src/main/java/com/example/cybercardv02/fragmentAdapter.java
@@ -4,17 +4,43 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+/**
+ * <h1>QR Code</h1>
+ *
+ * @author  Blane Staskiewicz
+ * @version 2.0
+ * @since   2021-07-28
+ */
 public class fragmentAdapter extends FragmentPagerAdapter {
     int mNumOfTabs;
-
+    
+    /**
+     * Constructor initializes adapter with number of tabs
+     *
+     * @param  fm         Fragment Manager
+     * @param  NoofTabs   Number of Tabs 
+     */
     public fragmentAdapter(@NonNull FragmentManager fm, int NoofTabs) {
         super(fm);
         this.mNumOfTabs = NoofTabs;
     }
+    
+    /**
+     * This method returns number of tabs
+     *
+     * @return  mNumOfTabs  Number of tabs
+     */
     @Override
     public int getCount() {
         return mNumOfTabs;
     }
+    
+    /**
+     * This method returns a Tab based off of position passed
+     *
+     * @param   position   Value (0-3) for each tab
+     * @return  new Tab()  New Tab based on position value
+     */
     @NonNull
     @Override
     public Fragment getItem(int position){
