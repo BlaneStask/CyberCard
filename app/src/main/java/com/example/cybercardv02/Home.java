670b3b928ad7a6268b3efa74a18dc1692a199600
Blane Staskiewicz <bas6248@psu.edu>
2021-07-28 11:36:09 -0400

    Update Home.java

--- app/src/main/java/com/example/cybercardv02/Home.java
+++ app/src/main/java/com/example/cybercardv02/Home.java
@@ -7,12 +7,27 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+/**
+ * <h1>Home</h1>
+ *
+ * @author  Blane Staskiewicz
+ * @version 2.0
+ * @since   2021-07-28
+ */
 public class Home extends Fragment {
     /*
     public fragment1() {
         super(R.layout.fragment1);
     }
     */
+    
+    /**
+     * This method creates Home View
+     *
+     * @param  inflater             used to inflate layout
+     * @param  viewGroup            view of Home tab
+     * @param  savedInstanceState   state when oriented
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState){
         return inflater.inflate(R.layout.fragment1, viewGroup, false);
